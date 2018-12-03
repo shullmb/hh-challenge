bb4a22ed3cc3bbc3925df4a93c0db02625b6dabe
Michael Shull <shullmb@gmail.com>
2018-12-02 18:08:42 -0800

    style and debug page numbers

--- client/src/components/CatalogContainer.jsx
+++ client/src/components/CatalogContainer.jsx
@@ -5,11 +5,16 @@ import PageNumbers from './PageNumbers';
 const CatalogContainer = ({swatches, handleSwatchSelect, currentPage, selectPageNumber}) => {
   let numOfPages = Math.ceil(swatches.length / 12)
   let startingIndex = (currentPage - 1) * 12
-  let currentSwatches = swatches.splice(startingIndex, 12)
+  let currentSwatches = Array.from(swatches).splice(startingIndex, 12)
+  console.log({swatches: swatches.length, numOfPages, currentPage, startingIndex})
   return (
-    <div className="CatalogContainer">
-      {currentSwatches.map( (swatch,i) => <Swatch key={i} swatch={swatch} handleClick={ () => handleSwatchSelect(swatch)}/>)}
-      <PageNumbers numOfPages={numOfPages} currentPage={currentPage} selectPageNumber={selectPageNumber} />
+    <div>
+      <div className="CatalogContainer">
+        {currentSwatches.map( (swatch,i) => <Swatch key={i} swatch={swatch} handleClick={ () => handleSwatchSelect(swatch)}/>)}
+      </div>
+        <div id="PageNumberContainer">
+          <PageNumbers numOfPages={numOfPages} currentPage={currentPage} selectPageNumber={selectPageNumber} />
+        </div>
     </div>
   )
 }
--- client/src/components/PageNumbers.jsx
+++ client/src/components/PageNumbers.jsx
@@ -3,18 +3,15 @@ import React from 'react';
 const PageNumbers = ({numOfPages, currentPage, selectPageNumber}) => {
   let pageNumbers = [];
   for (let i = 1; i < numOfPages; i++) {
-    console.log(pageNumbers)
     pageNumbers.push(i)
   }
 
-  console.log(pageNumbers)
   return (
     <nav>
-      <ul style={{display: 'inline-block'}}>
         {pageNumbers.map( num => {
-          return <li onClick={() => selectPageNumber(num)}> {num} </li>
+          let numClass = num === currentPage ? 'ActivePage' : ''; 
+          return <p key={`page${num}`} className={numClass} onClick={() => selectPageNumber(num)}> {num} </p>
         })}
-      </ul>
     </nav>
   )
 }
